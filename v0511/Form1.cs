993f0d8b2ba4c37df06ebd6ba9741a09b5b5f455
I_wakamatsu <130728120+ichi-waka@users.noreply.github.com>
2023-05-11 15:02:21 +0900

    Initial commit

---
+++ v0511/Form1.cs
@@ -0,0 +1,15 @@
+namespace v0511
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("‚µ‚á‚¯");
+        }
+    }
+}
